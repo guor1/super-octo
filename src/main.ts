d929e9573d7658b877ee4fe446792e38139e4b4f
guorui1 <guorui1@111.com.cn>
2022-02-17 15:20:35 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 // register vue composition api globally
+// 开发阶段用用
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/display.css'
+
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import generatedRoutes from 'virtual:generated-pages'
@@ -31,6 +36,7 @@ import 'uno.css'
   // 安装插件
   Object.values(import.meta.globEager('./plugins/*.js')).map(i => i.install?.(context))
 
+  app.use(ElementPlus)
   app.use(router)
 
   await router.isReady()
