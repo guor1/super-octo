f4ac154d31c8acc569e7811492ae2483fc4462a5
guorui1 <guorui1@111.com.cn>
2022-02-17 14:39:35 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,8 @@ import Inspect from 'vite-plugin-inspect'
 import WindiCSS from 'vite-plugin-windicss'
 import AutoImport from 'unplugin-auto-import/vite'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import { presetAttributify, presetIcons, presetUno } from 'unocss'
+import Unocss from 'unocss/vite'
 
 export default defineConfig({
   resolve: {
@@ -55,6 +57,21 @@ export default defineConfig({
         }),
       ],
       dts: 'src/components.d.ts',
+    }),
+
+    // https://github.com/antfu/unocss
+    Unocss({
+      shortcuts: [
+        ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+        ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+      ],
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+          scale: 1.2,
+        }),
+      ],
     }),
 
     // https://github.com/antfu/vite-plugin-icons
