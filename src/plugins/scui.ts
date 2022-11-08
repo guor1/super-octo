1e8a97faa7324a4e3a0a206393259489488dddf3
guor <abguorui0928@163.com>
2022-11-09 07:23:23 +0800

    1

--- src/plugins/scui.ts
+++ src/plugins/scui.ts
@@ -1,5 +1,4 @@
 import http from '~/utils/request'
-import { permission, rolePermission } from '~/utils/permission'
 import config from '~/config'
 import api from '~/api'
 import type { UserModule } from '~/types'
@@ -9,8 +8,6 @@ export const install: UserModule = ({ app }) => {
   app.config.globalProperties.$CONFIG = config
   app.config.globalProperties.$HTTP = http
   app.config.globalProperties.$API = api
-  app.config.globalProperties.$AUTH = permission
-  app.config.globalProperties.$ROLE = rolePermission
   // 全局代码错误捕捉
   // app.config.errorHandler = errorHandler
 }
