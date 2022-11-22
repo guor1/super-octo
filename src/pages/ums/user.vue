e93690e59b1673005694d2170841da6de7cf7db9
guorui1 <guorui1@111.com.cn>
2022-11-22 09:06:01 +0800

    1

--- src/pages/ums/user.vue
+++ src/pages/ums/user.vue
@@ -1,45 +1,45 @@
 <script lang="ts" setup>
-import { getUserList } from "~/api/ums/user";
-import type { Pagination } from "~/types";
+import { getUserList } from '~/api/ums/user'
+import type { Pagination } from '~/types'
 
 // 查询表单
 const generateFormModel = () => {
   return {
-    username: "",
-  };
-};
-const formModel = ref(generateFormModel());
+    username: '',
+  }
+}
+const formModel = ref(generateFormModel())
 const resetForm = () => {
-  formModel.value = generateFormModel();
-};
+  formModel.value = generateFormModel()
+}
 // 分页参数
 const basePagination: Pagination = {
   current: 1,
   pageSize: 10,
   total: 0,
-};
+}
 const pagination = reactive({
   ...basePagination,
-});
+})
 
-const tableData = ref<any[]>([]);
+const tableData = ref<any[]>([])
 const fetchTableData = async () => {
   const { data } = await getUserList({
     ...pagination,
     ...formModel.value,
-  });
-  tableData.value = data.list;
-  pagination.total = data.totalCount;
-};
+  })
+  tableData.value = data.list
+  pagination.total = data.totalCount
+}
 
 const searchTableData = () => {
-  pagination.current = 1;
-  fetchTableData();
-};
+  pagination.current = 1
+  fetchTableData()
+}
 
 onMounted(() => {
-  fetchTableData();
-});
+  fetchTableData()
+})
 </script>
 
 <template>
@@ -51,16 +51,22 @@ onMounted(() => {
             <el-input v-model="formModel.username" placeholder="请输入用户名称" />
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="searchTableData"> 查询 </el-button>
-            <el-button @click="resetForm"> 重置 </el-button>
+            <el-button type="primary" @click="searchTableData">
+              查询
+            </el-button>
+            <el-button @click="resetForm">
+              重置
+            </el-button>
           </el-form-item>
         </el-form>
       </el-card>
       <el-card>
         <el-space fill class="mb-16px">
-          <el-button type="primary"> 新增 </el-button>
+          <el-button type="primary">
+            新增
+          </el-button>
         </el-space>
-        <el-table row-key="id" :data="tableData">
+        <el-table row-key="id" :data="tableData" class="mt-16px">
           <el-table-column prop="id" label="编号" width="180" />
           <el-table-column prop="username" label="用户名" width="180" />
           <el-table-column prop="name" label="姓名" />
